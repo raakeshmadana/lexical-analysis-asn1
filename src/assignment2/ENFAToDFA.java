f1c9174cd40583e2a1f5f816e29a5299f2ebb215
Raakesh Madana <madanaraakesh@gmail.com>
2018-09-16 15:41:07 -0500

    Compute e-closure

---
+++ src/assignment2/ENFAToDFA.java
@@ -0,0 +1,52 @@
+package assignment2;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class ENFAToDFA {
+    
+    public static Character epsilon = '\u03b5';
+
+    public static Set<Integer> getEClosure(List<List<Edge>> DFAAdjList, List<Integer> states) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        Set<Integer> eClosure = new HashSet<Integer>();
+
+        // Push all states into stack
+        for(int i = 0; i < states.size(); i++) {
+            stack.addFirst(states.get(i));
+        }
+
+        while(!stack.isEmpty()) {
+            int state = stack.removeFirst();
+            List<Edge> edgeList = DFAAdjList.get(state);
+            for(int i = 0; i < edgeList.size(); i++) {
+                Edge edge = edgeList.get(i);
+                if(edge.transition == epsilon) {
+                    if(!eClosure.contains(edge.state)) {
+                        eClosure.add(edge.state);
+                        stack.addFirst(edge.state);
+                    }
+                }
+            }
+        }
+
+        return eClosure;
+    }
+
+    public static Set<Integer> move(List<List<Edge>> DFAAdjList, List<Integer> states, Character inputSymbol) {
+        Set<Integer> result = new HashSet<Integer>();
+        for(int i = 0; i < states.size(); i++) {
+            int state = states.get(i);
+            List<List<Edge>> edgeList = DFAAdjList.get(state);
+            for(int j = 0; j < edgeList.size(); j++) {
+                Edge edge = edgeList.get(i);
+                if(edge.transition == inputSymbol) {
+                    result.add(edge.state);
+                }
+            }
+        }
+    }
+}
